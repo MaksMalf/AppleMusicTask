6fe9757d52784a7299aaf1f3e88d784d0663d1c9
MaksMalf <maksim.malofeev995@gmail.com>
2022-09-06 22:34:08 +0300

    Initial Commit

---
+++ AppleMusicTask/AppleMusicTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicTaskApp.swift
+//  AppleMusicTask
+//
+//  Created by Maksim Malofeev on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
